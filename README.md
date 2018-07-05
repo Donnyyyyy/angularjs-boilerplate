3f37c694ebaca6c613f006bf618760806025dd82
Dmitrii Donetskii <don.skoy.333@gmail.com>
2018-07-05 19:52:52 +0300

    store docs

--- README.md
+++ README.md
@@ -39,4 +39,22 @@ go to [http://localhost:3000](http://localhost:3000) in your browser.
 
 This boilerplate based on [angularjs-webpack](https://github.com/preboot/angularjs-webpack).
 
-To be continued
+# Table of Contents
+
+* [Architecture](#architecture)
+    * [Store](#store)
+* [License](#license)
+
+# Architecture
+
+## Store
+
+Store is a service where all your app data is stored.
+
+- _``$store.createEntity(name, initialState)``_ - creates store entity with given ``name`` and ``initialState``.
+- _``$store.getEntity(name)``_ - retrieves an entity by ``name``.
+- _``$store.setEntity(name, newState)``_ - replaces an entity by ``name`` to ``newState``.
+- _``$store.updateEntity(name, newState)``_ - updates an entity by ``name`` with ``newState``. Different from ``setEntity`` by using ``Object.assign(oldState, newState)`` there.
+- _``$store.observeEntity(name, callback, scope, obj)``_ - start watching entity change: ``callback`` is called as entity is changed via ``setEntity`` or ``updateEntity``. ``scope`` object is used to apply changes after calling the callback; ``obj`` used to stop observing when ``obj`` is destroyed. Returns obserbing ``id`` used to stop obserbing.
+- _``$store.stopObserving(name, id)``_ - stop observing the entity value change. ``id`` is a obserbing id given when from ``observeEntity`` function return.
+- _``$store.removeEntity(name)``_ - removes entity by ``name``.
--- src/services/store.service.js
+++ src/services/store.service.js
@@ -1,6 +1,9 @@
 const AVADA_KEDAVRA = ['{}', '[]', 'undefined', '""'];
 
 
+/**
+ * @class StoreService is a service where all your app data is stored.
+ */
 export class StoreService {
   constructor($id) {
     this.$id = $id;
@@ -22,6 +25,17 @@ export class StoreService {
     return array;
   }
 
+  /**
+   * Start watching entity change: ``callback`` is called as entity is changed via ``setEntity`` or ``updateEntity``.
+   * 
+   * Returns obserbing ``id`` used to stop obserbing.
+   * 
+   * @param {string} name entity ``name`` to be observed
+   * @param {function} callback function 
+   * @param {*} scope watcher ``scope`` to apply changes after calling the callback
+   * @param {*} obj object used to stop observing when ``obj`` is destroyed.
+   * @memberof StoreService
+   */
   observeEntity(name, callback, scope, obj) {
     if (this.subscribers[name] === undefined) {
       this.subscribers[name] = [];
@@ -43,10 +57,24 @@ export class StoreService {
     return id;
   }
 
+  /**
+   * Stop observing the entity changes.
+   *
+   * @param {string} name entity ``name`` to stop observing.
+   * @param {id} id obserbing id given when from ``observeEntity`` function return.
+   * @memberof StoreService
+   */
   stopObserving(name, id) {
     this.subscribers[name] = StoreService.removeFromList(this.subscribers[name], id);
   }
 
+  /**
+   * Replaces an entity by ``name`` to ``newState``.
+   *
+   * @param {string} name entity ``name`` to be set 
+   * @param {*} newState new entity value
+   * @memberof StoreService
+   */
   setEntity(name, newState) {
     console.debug(name, newState);
 
@@ -65,6 +93,15 @@ export class StoreService {
     return newState;
   }
 
+  /**
+   * Updates an entity by ``name`` with ``newState``.
+   * 
+   * Different from ``setEntity`` by using ``Object.assign(oldState, newState)`` there.
+   *
+   * @param {string} name entity ``name`` to be updated
+   * @param {*} newState new entity value
+   * @memberof StoreService
+   */
   updateEntity(name, newState) {
     try {
       return this.setEntity(
@@ -79,15 +116,34 @@ export class StoreService {
     }
   }
 
+  /**
+   * Removes entity by ``name``
+   *
+   * @param {string} name entity name to be removed
+   * @memberof StoreService
+   */
   removeEntity(name) {
     this.entities[name] = undefined;
     this.subscribers[name] = undefined;
   }
 
+  /**
+   * Retrieves an entity by ``name``
+   *
+   * @param {string} name entity ``name`` to be retrieved
+   * @memberof StoreService
+   */
   getEntity(name) {
     return this.entities[name];
   }
 
+  /**
+   * Creates store entity with given ``name`` and ``initialState``
+   *
+   * @param {string} ``name`` entity name
+   * @param {*} initialState initial entity value
+   * @memberof StoreService
+   */
   createEntity(name, initialState) {
     if (this.entities[name] !== undefined) {
       return;
